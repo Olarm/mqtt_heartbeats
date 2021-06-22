fb975b1bc4e548215a4441ace20c95d44ac9de5f
Ola Rasmus Moen <ola.moen@wellstarter.com>
2021-06-22 11:37:10 +0200

    pub abd sub

--- heartbeat.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-mosquitto_pub -h 192.168.0.5 -t heartbeat/torino -m 1
---
+++ heartbeat_pub.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+[ "$#" -eq 1 ] || die "Client name required, $# provided"
+
+mosquitto_pub -h 192.168.0.5 -t heartbeat/torino -m 1
